021798fe597b912c37a0cf161ae8a74dd1200894
NTDLip-2401 <datnguyentien2401@gmail.com>
2026-05-29 14:09:47 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),   
   ],
-  base: '/FERedmineApp/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
